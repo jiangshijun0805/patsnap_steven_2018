2687f11255954b95f09ffe8d00ca51421437c9bc
xiaojiang <674515895@qq.com>
2018-03-02 21:30:11 +0800

    first commit for test

---
+++ src/main/java/com/xiaojiang/beans/User.java
@@ -0,0 +1,31 @@
+package com.xiaojiang.beans;
+
+public class User {
+    private String name;
+    private String password;
+    private Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
